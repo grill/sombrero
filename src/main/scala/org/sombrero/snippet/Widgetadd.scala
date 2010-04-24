98f3d2e69362c24333f925eb46b649d4d8622364
Alexander Steiner <alex@Zebes.(none)>
2010-04-24 18:35:11 +0200

    moved the rest of util.WidgetAdd to snippet.Widgetadd, Widgetadd.newData for parameterized widget adds (for device discovery)

--- src/main/scala/org/sombrero/snippet/Widgetadd.scala
+++ src/main/scala/org/sombrero/snippet/Widgetadd.scala
@@ -4,15 +4,44 @@ import scala.xml._
 import org.sombrero.model
 import org.sombrero.util.WidgetAdd
 import net.liftweb.util._
+import net.liftweb.http.RequestVar
 
 class Widgetadd {
-  def render(ignore : NodeSeq) : NodeSeq = model.Room.current match {
+  def render(ignore : NodeSeq) : NodeSeq = render(ignore, false)
+  /*
+  model.Room.current match {
     case Full(room) => WidgetAdd.render(room)(ignore)
     case _ =>  WidgetAdd.render(ignore)
   }
+  */
   
-  def frame(ignore : NodeSeq) : NodeSeq = model.Room.current match {
+  def render(ignore : NodeSeq, inFrame : Boolean) : NodeSeq = Widgetadd.newData.is match {
+    case Full(wd) =>
+      model.Widget.create.completeForm(
+        wd,
+        "Save Widget",
+        (w, wd : model.KNXWidget) => w.room(model.Room.current),
+        if(inFrame) "/closeframe"
+          else model.Room.current.map("/room/" + _.id.is) openOr "/"
+      )
+    case _ =>
+      model.Widget.create.completeForm(
+        "Save Widget",
+        (w, wd) => w.room(model.Room.current),
+        if(inFrame) "/closeframe"
+          else model.Room.current.map("/room/" + _.id.is) openOr "/"
+      )
+  }
+  
+  def form(ignore : NodeSeq) : NodeSeq = render(ignore, true)
+  /*
+  model.Room.current match {
     case Full(room) => WidgetAdd.render(room, ignore, true)
     case _ =>  WidgetAdd.render(ignore, true)
   }
+  */
+}
+
+object Widgetadd {
+  object newData extends RequestVar[Box[model.KNXWidget]](Empty)
 }
